34846b4e8e86bda2887c9fafbc8c2970cd9d7693
Steven Harman <steveharman@gmail.com>
2011-10-06 18:24:06 -0400

    The Cellar!

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source :rubygems
+
+gem 'rails', '3.1.0'
+gem 'jquery-rails'
+
+group :assets do
+  gem 'less-rails-bootstrap'
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+group :production do
+  gem 'pg'
+end
+
+group :development, :test do
+  gem 'sqlite3'
+  gem 'rspec-rails', '~> 2.6'
+#  gem 'shoulda-matchers'
+#  gem 'factory_girl_rails'
+#  gem 'heroku'
+#  gem 'capybara'
+#  gem 'simplecov'
+end
+
+# To use debugger
+# gem 'ruby-debug19', :require => 'ruby-debug'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cellar::Application.config.secret_token = 'da68736412dda047bf7ba1dc28eba5c63767f82957d9a370d1d902da56ad48b8432389753ff34b8317c01b5f48d55ba68fc2be07f5eb42bea3dd1005ee7d820b'
