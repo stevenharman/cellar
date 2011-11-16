93eb1e0ffbc332c93f01a5434c335c7f365ac7f6
Steven Harman <steveharman@gmail.com>
2011-11-16 00:29:31 -0500

    redirect to sign_in_path if required_login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  protected
+
+  def not_authenticated
+    redirect_to sign_in_path, :alert => "Please sign in first."
+  end
 end
