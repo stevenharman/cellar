03a7462f02ce6c9cacd2ea73270c3246d2eace23
Steven Harman <steveharman@gmail.com>
2011-11-22 16:59:12 -0500

    link to user profile from topbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,9 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   validates :username, uniqueness: true, presence: true
   validates :password, presence: { on: :create }
+
+  def to_param
+    username
+  end
+
 end
