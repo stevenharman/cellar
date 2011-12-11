249138345893be6b1636f6b7c4f00ed8ad29a3dc
Steven Harman <steveharman@gmail.com>
2011-12-10 22:21:11 -0500

    Barback can fetch beers from a cellar

--- app/cellar/barback.rb
+++ app/cellar/barback.rb
@@ -1,14 +1,19 @@
 class Barback
 
-  def initialize(fetch_brews=Brew)
+  def initialize(fetch_brews=Brew, fetch_beers=Beer)
     @fetch_brews = fetch_brews
+    @fetch_beers = fetch_beers
   end
 
   def brews_from_cellar(keeper)
     fetch_brews.from_cellar(keeper)
   end
 
+  def beers_from_cellar_for_brew(keeper, brew)
+    fetch_beers.from_cellar(keeper, brew: brew)
+  end
+
   private
 
-  attr_reader :fetch_brews
+  attr_reader :fetch_brews, :fetch_beers
 end
--- spec_no_rails/cellar/barback_spec.rb
+++ spec_no_rails/cellar/barback_spec.rb
@@ -3,15 +3,26 @@ app_require 'app/cellar/barback'
 
 describe Barback do
   let(:fetch_brews) { double("Brew") }
-  let(:barback) { Barback.new(fetch_brews) }
+  let(:fetch_beers) { double("Beer") }
+  let(:barback) { Barback.new(fetch_brews, fetch_beers) }
+  let(:bob) { double("Keeper") }
 
   describe "#brews_from_cellar" do
-    let(:brews) { double("Brews from Cellar") }
-    let(:keeper) { double("Keeper") }
+    let(:brews) { double("Brews from the Cellar") }
 
     it "fetches brews from the keeper's cellar" do
-      fetch_brews.stub(:from_cellar).with(keeper).and_return(brews)
-      barback.brews_from_cellar(keeper).should == brews
+      fetch_brews.stub(:from_cellar).with(bob).and_return(brews)
+      barback.brews_from_cellar(bob).should == brews
+    end
+  end
+
+  describe "#beers_for_brew_from_cellar" do
+    let(:brew) { double("Brew") }
+    let(:beers) { double("Beers from the Cellar") }
+
+    it "fetches beers from the keeper's cellar, for a specific brew" do
+      fetch_beers.stub(:from_cellar).with(bob, {brew: brew}).and_return(beers)
+      barback.beers_from_cellar_for_brew(bob, brew).should == beers
     end
   end
 end
