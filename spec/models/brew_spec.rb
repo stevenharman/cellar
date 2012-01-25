689a10b2b0f47d31fa5477965babe8b3e6fe09ec
Steven Harman <steveharman@gmail.com>
2012-01-24 22:05:53 -0500

    faster brew specs w/DatabaseCleaner

--- spec/models/brew_spec.rb
+++ spec/models/brew_spec.rb
@@ -27,31 +27,34 @@ describe Brew do
   it { should allow_mass_assignment_of(:description) }
 
   describe "scope .with_beers" do
-    let(:empty_brew) { FactoryGirl.create(:brew) }
+    let!(:empty_brew) { FactoryGirl.create(:brew) }
     before do
       FactoryGirl.create_list(:beer, 2, brew: FactoryGirl.create(:brew))
-      empty_brew
       FactoryGirl.create(:beer)
     end
 
-    it "includes brews that have beers" do
-      Brew.with_beers.all.size.should == 2
-    end
-
-    it "excludes brews with no beers" do
-      Brew.with_beers.all.should_not include(empty_brew)
+    it "includes only brews that have beers" do
+      brews_with_beers = Brew.with_beers.all
+      brews_with_beers.should_not include(empty_brew)
+      brews_with_beers.size.should == 2
     end
   end
 
   describe ".from_cellar" do
     let(:bob) { FactoryGirl.create(:user) }
     let(:cellared_brews) { Brew.from_cellar(bob) }
-    before do
+
+    before(:all) do
+      DatabaseCleaner.strategy = :truncation
+      DatabaseCleaner.start
+
       @bobs_brew_1 = FactoryGirl.create(:beer, user: bob).brew
       @other_brew = FactoryGirl.create(:beer).brew
       @bobs_brew_2 = FactoryGirl.create(:beer, user: bob).brew
       @bobs_skunked_brew = FactoryGirl.create(:beer, :skunked, user:bob).brew
     end
+
+    after(:all) { DatabaseCleaner.clean }
 
     it "includes brews from the user's cellar" do
       cellared_brews.should =~ [@bobs_brew_1, @bobs_brew_2]
