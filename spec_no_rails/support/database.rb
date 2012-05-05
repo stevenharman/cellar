b7e71f8c08c0a7e62ece58aef093fb4751a5f5d3
Steven Harman <steveharman@gmail.com>
2012-05-04 22:01:58 -0400

    Kill dead support files

--- spec_no_rails/support/database.rb
+++
@@ -1,5 +0,0 @@
-require 'active_record'
-require 'yaml'
-
-dbconfig = YAML::load(File.open("#{File.dirname(__FILE__)}/../../config/database.yml"))
-ActiveRecord::Base.establish_connection(dbconfig['test'])
--- spec_no_rails/support/database_cleaner.rb
+++
@@ -1,6 +0,0 @@
-require 'database_cleaner'
-
-RSpec.configure do |config|
-  config.before(:each) { DatabaseCleaner.start }
-  config.after(:each) { DatabaseCleaner.clean }
-end
