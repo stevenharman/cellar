cff1944e7a08b03502d8d0162b2ecb822a5ca015
Steven Harman <steveharman@gmail.com>
2012-08-20 23:16:29 -0400

    Kill unused spec

--- spec/controllers/beers_controller_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-describe BeersController do
-  # Evil, I know. Currently restructring the app and this controller does
-  # little - though soon it will add new beers to the app.
-end
