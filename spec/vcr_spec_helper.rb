13e8c6f5aed2e0c4c68460e0f465b88d7283d51d
Steven Harman <steveharman@gmail.com>
2012-10-13 14:53:11 -0400

    Fix VCR deprecation warning

--- spec/vcr_spec_helper.rb
+++ spec/vcr_spec_helper.rb
@@ -7,7 +7,7 @@ end
 
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/vcr_cassettes'
-  c.stub_with :webmock
+  c.hook_into :webmock
   c.ignore_localhost = true
   c.configure_rspec_metadata!
   c.filter_sensitive_data('API_KEY') { ENV['BREWERY_DB_API_KEY'] }
