6bf15422026982375609b5f6e99ec7ead96326f6
Steven Harman <steveharman@gmail.com>
2012-12-09 08:29:58 -0500

    Fix Style factory; unique name per run

--- spec/factories/styles.rb
+++ spec/factories/styles.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :style do
-    name  { |n| "Style ##{n}" }
+    sequence(:name)  { |n| "Style ##{n}" }
     brewery_db_id { SecureRandom.random_number(999_999_999) }
     category
   end
