92aef1777e592840f6727cda21e8bf1925e1b55f
Steven Harman <steveharman@gmail.com>
2012-12-11 17:21:48 -0500

    Read local Redis port on demand

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,10 @@
-ENV['REDISTOGO_URL'] ||= 'redis://localhost:6380' # same as config/redis.conf
+local_redis_url = Proc.new do
+  config_file = File.join(File.expand_path('../..', __FILE__), 'redis.conf')
+  port = File.read(config_file).chomp.split.last
+  "redis://localhost:#{port}"
+end
+
+ENV['REDISTOGO_URL'] ||= local_redis_url.call
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDISTOGO_URL'], namespace: 'brewdega-cellar' }
