8d509a516f51452e3174b0c8651d142be5535a46
Steven Harman <steveharman@gmail.com>
2013-01-05 20:59:18 -0500

    Use custom connection pool size for Sidekiq
    * Because Rails prefers a `DATABASE_URL` environment variable to
      `database.yml`, we change the `DATABASE_URL` for the Sidekiq server
      process and then force `ActiveRecord::Base` to drop and re-establish a
      connection to Postgre with the new pool size.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -12,10 +12,15 @@ Sidekiq.configure_server do |config|
   database_url = ENV['DATABASE_URL']
   sidekiq_concurrency = ENV['SIDEKIQ_CONCURRENCY']
   if(database_url && sidekiq_concurrency)
-    Rails.logger.warn("Setting custom connection pool size of #{sidekiq_concurrency} for Sidekiq Server...")
+    Rails.logger.debug("Setting custom connection pool size of #{sidekiq_concurrency} for Sidekiq Server")
+
     ENV['DATABASE_URL'] = "#{database_url}?pool=#{sidekiq_concurrency}"
-    Rails.logger.warn("DATABASE_URL = #{ENV['DATABASE_URL']}")
+    Rails.logger.debug(%Q(DATABASE_URL => "#{ENV['DATABASE_URL']}"))
+
+    ActiveRecord::Base.establish_connection
   end
+
+  Rails.logger.info("Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
 end
 
 Sidekiq.configure_client do |config|
