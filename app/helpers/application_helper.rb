cdd40cbae33744effcbd4696c8206ae027eecb39
Steven Harman <steveharman@gmail.com>
2013-01-10 15:35:05 -0500

    Use pretty dates for beers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
     @search_query ||= Search::Query.new
   end
 
+  def ldate(date, options = {})
+    if(date)
+      content_tag(:time, l(date, options), datetime: date.iso8601)
+    end
+  end
+
 end
