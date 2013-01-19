32f9134ac5c4a2e7363edc3f0ba5453dd05d9099
Steven Harman <steveharman@gmail.com>
2013-01-19 18:23:38 -0500

    Configure production to use Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,4 +60,15 @@ BrewdegaCellar::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.default_url_options = { :host => 'cellar.brewdega.com' }
+
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+
+  config.action_mailer.delivery_method = :smtp
 end
