ff5379bd28e413d425c7eeb2a01b3c646800006b
Steven Harman <steveharman@gmail.com>
2013-02-06 12:56:05 -0500

    Cleanup comments on a few configuration options

--- config/initializers/brewery_db.rb
+++ config/initializers/brewery_db.rb
@@ -1,1 +1,2 @@
+# default to a limited capability test key
 ENV['BREWERY_DB_API_KEY'] ||= '2a3e944b3fcc18c0617ea642c9edb5dd'
--- config/initializers/supply_chain_job.rb
+++ config/initializers/supply_chain_job.rb
@@ -1,1 +1,2 @@
+# pre-load all of the Jobs
 Dir[File.join(Rails.root, %w[app models supply_chain job *])].each { |f| require f }
