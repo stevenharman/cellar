610981f0f7d3f1fe97fb97a91fdecc1ee0de218a
Steven Harman <steveharman@gmail.com>
2013-03-10 14:30:12 -0400

    AnonymousUser never has cellared beers

--- app/models/anonymous_user.rb
+++ app/models/anonymous_user.rb
@@ -1,7 +1,14 @@
+require 'models/beer'
+
 class AnonymousUser
   attr_reader :username
 
   def active?
     false
+  end
+
+  def cellared_beers
+    # Rails 4: use Beer.none
+    Beer.where(false)
   end
 end
--- spec/models/anonymous_user_spec.rb
+++ spec/models/anonymous_user_spec.rb
@@ -1,7 +1,10 @@
+require 'active_record_spec_helper'
 require 'models/anonymous_user'
 
 describe AnonymousUser do
   subject(:user) { AnonymousUser.new }
 
   it { expect(user).not_to be_active }
+
+  it { expect(user.cellared_beers).to be_empty }
 end
