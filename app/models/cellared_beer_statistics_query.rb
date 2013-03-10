f1c306fe0a5b1276a338f4d17b888c1c17f07fea
Steven Harman <steveharman@gmail.com>
2013-03-10 12:26:44 -0400

    Document the query object

--- app/models/cellared_beer_statistics_query.rb
+++ app/models/cellared_beer_statistics_query.rb
@@ -1,6 +1,20 @@
 require 'delegate'
 
-class CellaredBeerStatisticsQuery < SimpleDelegator
+# Public: A query object to gather the count of currently cellared beers, per
+# brew, in the keeper's cellar.
+#
+# Returns a hash with the Brew#id as the key and the count of currently
+# cellared beers as the value.
+#
+# Examples
+#
+#   CellaredBeerStatisticsQuery.query(keeper)
+#   #=> {
+#         #brew_id => count
+#         867      => 2,
+#         5309     => 2,
+#       }
+class CellaredBeerStatisticsQuery < DelegateClass(User)
 
   def self.query(keeper)
     new(keeper)
