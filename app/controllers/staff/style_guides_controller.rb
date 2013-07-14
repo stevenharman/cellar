6c7e8848baa887d0de816cd002504f59c028beed
Steven Harman <steveharman@gmail.com>
2013-07-14 18:38:46 -0400

    Flash only applies now, for style_guide

--- app/controllers/staff/style_guides_controller.rb
+++ app/controllers/staff/style_guides_controller.rb
@@ -2,9 +2,9 @@ module Staff
   class StyleGuidesController < ApplicationController
 
     def show
-      flash[:success] = "Yay! This is what a :success looks like!"
-      flash[:notice] = "Hey there! This is what a :notice looks like."
-      flash[:error] = "Oh no... this is what an :error looks like!"
+      flash.now[:success] = "Yay! This is what a :success looks like!"
+      flash.now[:notice] = "Hey there! This is what a :notice looks like."
+      flash.now[:error] = "Oh no... this is what an :error looks like!"
     end
 
   end
