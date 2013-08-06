f95466b2d3a6d176d3616b68c6238b64be02f1c0
Steven Harman <steveharman@gmail.com>
2013-08-06 15:11:46 -0400

    Update schema.rb for Ruby 2/Rails 4

--- db/schema.rb
+++ db/schema.rb
@@ -9,84 +9,89 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(:version => 20130724011027) do
 
-  create_table "beers", :force => true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "beers", force: true do |t|
     t.date     "best_by"
     t.integer  "brew_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
-    t.string   "status",     :default => "cellared"
+    t.string   "status",     default: "cellared"
     t.text     "notes"
     t.integer  "vintage"
   end
 
-  add_index "beers", ["brew_id"], :name => "index_beers_on_brew_id"
-  add_index "beers", ["status"], :name => "index_beers_on_status"
-  add_index "beers", ["user_id"], :name => "index_beers_on_user_id"
+  add_index "beers", ["brew_id"], name: "index_beers_on_brew_id", using: :btree
+  add_index "beers", ["status"], name: "index_beers_on_status", using: :btree
+  add_index "beers", ["user_id"], name: "index_beers_on_user_id", using: :btree
 
-  create_table "breweries", :force => true do |t|
+  create_table "breweries", force: true do |t|
     t.string   "name"
     t.string   "website"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "brewery_db_id",                            :null => false
+    t.string   "brewery_db_id",                         null: false
     t.text     "description"
     t.date     "established"
     t.boolean  "organic"
     t.text     "images"
-    t.string   "brewery_db_status", :default => "unknown", :null => false
+    t.string   "brewery_db_status", default: "unknown", null: false
   end
 
-  add_index "breweries", ["brewery_db_id"], :name => "index_breweries_on_brewery_db_id", :unique => true
-  add_index "breweries", ["brewery_db_status"], :name => "index_breweries_on_brewery_db_status"
+  add_index "breweries", ["brewery_db_id"], name: "index_breweries_on_brewery_db_id", unique: true, using: :btree
+  add_index "breweries", ["brewery_db_status"], name: "index_breweries_on_brewery_db_status", using: :btree
 
-  create_table "brewery_brews", :force => true do |t|
-    t.integer  "brewery_id", :null => false
-    t.integer  "brew_id",    :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "brewery_brews", force: true do |t|
+    t.integer  "brewery_id", null: false
+    t.integer  "brew_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "brewery_brews", ["brew_id"], :name => "index_brewery_brews_on_brew_id"
-  add_index "brewery_brews", ["brewery_id", "brew_id"], :name => "index_brewery_brews_on_brewery_id_brew_id", :unique => true
+  add_index "brewery_brews", ["brew_id"], name: "index_brewery_brews_on_brew_id", using: :btree
+  add_index "brewery_brews", ["brewery_id", "brew_id"], name: "index_brewery_brews_on_brewery_id_brew_id", unique: true, using: :btree
 
-  create_table "brews", :force => true do |t|
+  create_table "brews", force: true do |t|
     t.string   "name"
-    t.decimal  "abv",                  :precision => 5, :scale => 2
+    t.decimal  "abv",                  precision: 5, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "description"
-    t.decimal  "ibu",                  :precision => 5, :scale => 2
-    t.string   "brewery_db_id",                                                             :null => false
+    t.decimal  "ibu",                  precision: 5, scale: 2
+    t.string   "brewery_db_id",                                                    null: false
     t.integer  "style_id"
     t.text     "labels"
     t.date     "year"
     t.boolean  "organic"
     t.string   "base_brew_id"
     t.decimal  "original_gravity"
-    t.integer  "cellared_beers_count",                               :default => 0,         :null => false
-    t.string   "brewery_db_status",                                  :default => "unknown", :null => false
+    t.integer  "cellared_beers_count",                         default: 0,         null: false
+    t.string   "brewery_db_status",                            default: "unknown", null: false
+    t.integer  "brew_availability_id"
   end
 
-  add_index "brews", ["brewery_db_id"], :name => "index_brews_on_brewery_db_id", :unique => true
-  add_index "brews", ["brewery_db_status"], :name => "index_brews_on_brewery_db_status"
-  add_index "brews", ["style_id"], :name => "index_brews_on_style_id"
+  add_index "brews", ["brew_availability_id"], name: "index_brews_on_brew_availability_id", using: :btree
+  add_index "brews", ["brewery_db_id"], name: "index_brews_on_brewery_db_id", unique: true, using: :btree
+  add_index "brews", ["brewery_db_status"], name: "index_brews_on_brewery_db_status", using: :btree
+  add_index "brews", ["style_id"], name: "index_brews_on_style_id", using: :btree
 
-  create_table "categories", :force => true do |t|
-    t.string   "name",          :null => false
-    t.integer  "brewery_db_id", :null => false
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+  create_table "categories", force: true do |t|
+    t.string   "name",          null: false
+    t.integer  "brewery_db_id", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
-  add_index "categories", ["brewery_db_id"], :name => "index_categories_on_brewery_db_id", :unique => true
-  add_index "categories", ["name"], :name => "index_categories_on_name", :unique => true
+  add_index "categories", ["brewery_db_id"], name: "index_categories_on_brewery_db_id", unique: true, using: :btree
+  add_index "categories", ["name"], name: "index_categories_on_name", unique: true, using: :btree
 
-  create_table "pg_search_documents", :force => true do |t|
+  create_table "pg_search_documents", force: true do |t|
     t.text     "content"
     t.integer  "searchable_id"
     t.string   "searchable_type"
@@ -94,29 +99,29 @@ ActiveRecord::Schema.define(:version => 20130724011027) do
     t.datetime "updated_at"
   end
 
-  create_table "styles", :force => true do |t|
-    t.string   "name",          :null => false
-    t.integer  "brewery_db_id", :null => false
-    t.integer  "category_id",   :null => false
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+  create_table "styles", force: true do |t|
+    t.string   "name",          null: false
+    t.integer  "brewery_db_id", null: false
+    t.integer  "category_id",   null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.text     "description"
   end
 
-  add_index "styles", ["brewery_db_id"], :name => "index_styles_on_brewery_db_id", :unique => true
-  add_index "styles", ["category_id"], :name => "index_styles_on_category_id"
-  add_index "styles", ["name"], :name => "index_styles_on_name", :unique => true
+  add_index "styles", ["brewery_db_id"], name: "index_styles_on_brewery_db_id", unique: true, using: :btree
+  add_index "styles", ["category_id"], name: "index_styles_on_category_id", using: :btree
+  add_index "styles", ["name"], name: "index_styles_on_name", unique: true, using: :btree
 
-  create_table "users", :force => true do |t|
-    t.string   "username",                                  :null => false
-    t.string   "email",                  :default => "",    :null => false
+  create_table "users", force: true do |t|
+    t.string   "username",                               null: false
+    t.string   "email",                  default: "",    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "encrypted_password",     :default => "",    :null => false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -129,13 +134,13 @@ ActiveRecord::Schema.define(:version => 20130724011027) do
     t.string   "website"
     t.string   "location"
     t.string   "name"
-    t.boolean  "staff",                  :default => false, :null => false
+    t.boolean  "staff",                  default: false, null: false
   end
 
-  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-  add_index "users", ["staff"], :name => "index_users_on_staff"
-  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["staff"], name: "index_users_on_staff", using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
