1edfd6d1546fa468afa3ca63ea46bc59e04486a6
Steven Harman <steveharman@gmail.com>
2013-08-06 15:47:08 -0400

    Update VCR cassettes

--- spec/models/supply_chain/warehouse_spec.rb
+++ spec/models/supply_chain/warehouse_spec.rb
@@ -7,14 +7,14 @@ describe SupplyChain::Warehouse do
   describe 'fetching from the API', :vcr do
 
     it 'fetches categories from BreweryDB' do
-      categories = subject.categories
+      categories = warehouse.categories
       expect(categories.first.id).to be_kind_of Fixnum
       expect(categories.each).to be_kind_of Enumerator
       expect(categories.count).to eq(12)
     end
 
     it 'fetches styles from BreweryDB' do
-      styles = subject.styles
+      styles = warehouse.styles
       expect(styles.first.id).to be_kind_of Fixnum
       expect(styles.first.category_id).to be_kind_of Fixnum
       expect(styles.each).to be_kind_of Enumerator
@@ -29,26 +29,26 @@ describe SupplyChain::Warehouse do
     end
 
     it 'fetches breweries from BreweryDB', :slow do
-      breweries = subject.breweries
+      breweries = warehouse.breweries
       expect(breweries.first.id).to be_kind_of String
       expect(breweries.each).to be_kind_of Enumerator
       expect(breweries.count).to eq(4567)
     end
 
     it 'fetches a single brewery' do
-      brewery = subject.brewery('Idm5Y5')
+      brewery = warehouse.brewery('Idm5Y5')
       expect(brewery.id).to eq('Idm5Y5')
     end
 
     it "fetches a brewery's beers" do
-      brews = subject.brews_for_brewery('Idm5Y5')
+      brews = warehouse.brews_for_brewery('Idm5Y5')
       expect(brews.first.id).to be_kind_of String
       expect(brews.each).to be_kind_of Enumerator
       expect(brews.count).to eq(25)
     end
 
     it 'fetches a single brew' do
-      brew = subject.brew('VvFIVD')
+      brew = warehouse.brew('VvFIVD')
       expect(brew.id).to eq('VvFIVD')
       expect(brew.name).to eq('Hopslam Ale')
     end
@@ -59,12 +59,12 @@ describe SupplyChain::Warehouse do
 
     it 'fetchs an empty list of categories rather than nil' do
       client.stub_chain(:categories, :all) { nil }
-      expect(subject.categories).to be_empty
+      expect(warehouse.categories).to be_empty
     end
 
     it 'fetchs an empty list of styles rather than nil' do
       client.stub_chain(:styles, :all) { nil }
-      expect(subject.styles).to be_empty
+      expect(warehouse.styles).to be_empty
     end
 
     it 'fetchs an empty list of beer availabilities rather than nil' do
@@ -74,12 +74,12 @@ describe SupplyChain::Warehouse do
 
     it 'fetchs an empty list of breweries rather than nil' do
       client.stub_chain(:breweries, :all) { nil }
-      expect(subject.breweries).to be_empty
+      expect(warehouse.breweries).to be_empty
     end
 
     it 'fetches an empty list of beers rather than a nil' do
       client.stub_chain(:brewery, :beers) { nil }
-      expect(subject.brews_for_brewery('Idm5Y5')).to be_empty
+      expect(warehouse.brews_for_brewery('Idm5Y5')).to be_empty
     end
   end
 end
