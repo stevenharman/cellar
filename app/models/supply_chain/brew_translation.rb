9e04eace8756525f55fe470be2ed8fb85013bbf6
Steven Harman <steveharman@gmail.com>
2013-08-12 22:12:26 -0400

    Use WarehouseMap in translations
    to consolidate the surface area of looking up all of the various
    entities in the system

--- app/models/supply_chain/brew_translation.rb
+++ app/models/supply_chain/brew_translation.rb
@@ -4,41 +4,49 @@ module SupplyChain
   class BrewTranslation
     include SupplyChain::Conversions
 
-    def initialize(brew, brewery_factory = ::Brewery)
+    attr_reader :brew, :warehouse_map
+
+    def initialize(brew, warehouse_map = WarehouseMap.new)
       @brew = brew
-      @brewery_factory = brewery_factory
+      @warehouse_map = warehouse_map
     end
 
     def translate(raw_data)
-      @brew.tap do |b|
-        b.name = raw_data.name
-        b.description = raw_data.description
-        b.style_id = raw_data.style_id
-        b.base_brew_id = raw_data.beer_variation_id
-        b.organic = boolean(raw_data.is_organic)
-        b.year = year(raw_data.year)
-        b.abv = decimal(raw_data.abv)
-        b.ibu = decimal(raw_data.ibu)
-        b.original_gravity = decimal(raw_data.original_gravity)
-        b.brewery_db_status = raw_data.status
+      brew.name = raw_data.name
+      brew.description = raw_data.description
+      brew.base_brew_id = raw_data.beer_variation_id
+      brew.organic = boolean(raw_data.is_organic)
+      brew.year = year(raw_data.year)
+      brew.abv = decimal(raw_data.abv)
+      brew.ibu = decimal(raw_data.ibu)
+      brew.original_gravity = decimal(raw_data.original_gravity)
+      brew.style = find_style(raw_data.style_id)
+      brew.brewery_db_status = raw_data.status
 
-        labels = images(raw_data.labels)
-        b.icon = labels.icon
-        b.medium_image = labels.medium
-        b.large_image = labels.large
-      end
+      labels = images(raw_data.labels)
+      brew.icon = labels.icon
+      brew.medium_image = labels.medium
+      brew.large_image = labels.large
 
-      @brew.breweries << new_breweries(raw_data.breweries)
-      @brew.save!
-      @brew
+      brew.breweries << new_breweries(raw_data.breweries)
+      brew.save!
+      brew
     end
 
     private
 
     def new_breweries(breweries)
       ids = Array(breweries).map(&:id)
-      all_breweries = @brewery_factory.find_by_brewery_db_ids(ids)
-      all_breweries - @brew.breweries
+      all_breweries = find_breweries(ids)
+      all_breweries - brew.breweries
+    end
+
+    def find_breweries(ids)
+      warehouse_map.find_breweries(ids)
+    end
+
+    def find_style(id)
+      warehouse_map.find_style(id)
     end
 
   end
--- app/models/supply_chain/style_translation.rb
+++ app/models/supply_chain/style_translation.rb
@@ -1,20 +1,26 @@
 module SupplyChain
   class StyleTranslation
 
-    def initialize(style, category_factory = ::Category)
+    attr_reader :style, :warehouse_map
+
+    def initialize(style, warehouse_map = WarehouseMap.new)
       @style = style
-      @category_factory = category_factory
+      @warehouse_map = warehouse_map
     end
 
     def translate(raw_data)
-      @style.tap do |s|
-        s.name = raw_data.name
-        s.description = raw_data.description
-        s.category = @category_factory.find_by_brewery_db_id(raw_data.category_id)
-      end
+      style.name = raw_data.name
+      style.description = raw_data.description
+      style.category = find_category(raw_data.category_id)
 
-      @style.save
-      @style
+      style.save
+      style
+    end
+
+    private
+
+    def find_category(id)
+      warehouse_map.find_category(id)
     end
 
   end
