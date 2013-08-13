38873cf373a7e26227ce5169f3812efd4264f107
Steven Harman <steveharman@gmail.com>
2013-08-12 22:45:25 -0400

    Show real availability

--- app/decorators/brew_decorator.rb
+++ app/decorators/brew_decorator.rb
@@ -5,6 +5,10 @@ class BrewDecorator < ApplicationDecorator
   delegate_all
   decorates_association :breweries
 
+  def availability_name
+    availability && availability.name
+  end
+
   def style_name
     style && style.name
   end
