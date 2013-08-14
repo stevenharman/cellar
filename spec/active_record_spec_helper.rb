4af32f1a342080d68e917cb5a2ba280acd586c0d
Steven Harman <steveharman@gmail.com>
2013-08-13 20:21:12 -0400

    Ensure DatabaseCleaner config happens together
    Without this the before hooks were running in the incorrect order.

--- spec/active_record_spec_helper.rb
+++ spec/active_record_spec_helper.rb
@@ -5,9 +5,23 @@ connection_info = YAML.load(ERB.new(File.read('config/database.yml')).result)['t
 ActiveRecord::Base.establish_connection(connection_info)
 
 RSpec.configure do |config|
+  # Let DatabaseCleaner handle this
+  config.use_transactional_fixtures = false
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
+  end
+
+  # Because Capybara runs in a separate thread with a different database
+  # connection, feature specs cannot use a transaction.
+  config.before(type: :feature) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  # Reset so other non-request specs don't have to deal with slow truncation.
+  config.after(type: :feature) do
+    DatabaseCleaner.strategy = :transaction
   end
 
   config.before(:each) do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,21 +13,8 @@ require_relative 'active_record_spec_helper'
 Dir[Rails.root.join('spec/support/**/*.rb')].each {|f| require f}
 
 RSpec.configure do |config|
-  config.use_transactional_fixtures = false # database_cleaner handles this
-
   config.before(:each) do
     ActionMailer::Base.deliveries.clear
-  end
-
-  # Request specs cannot use a transaction because Capybara runs in a separate
-  # thread with a different database connection.
-  config.before(type: :request) do
-    DatabaseCleaner.strategy = :truncation
-  end
-
-  # Reset so other non-request specs don't have to deal with slow truncation.
-  config.after(type: :request) do
-    DatabaseCleaner.strategy = :transaction
   end
 
   config.include Devise::TestHelpers, type: :controller
