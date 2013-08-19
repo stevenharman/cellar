32579063b95e3c5bca654aa39d5d940e09aa8108
Steven Harman <steveharman@gmail.com>
2013-08-19 01:37:52 -0400

    Use Gravatar to generate URL
    Also rely on a background image for missing gravatars.

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,12 +1,11 @@
-require 'digest/md5'
+require 'gravatar'
 
 class UserDecorator < ApplicationDecorator
   delegate_all
 
   def gravatar(size = 64)
-    hashed_email = Digest::MD5.hexdigest(email)
-    default_image = h.image_url('default-avatar.png')
-    h.image_tag("https://secure.gravatar.com/avatar/#{hashed_email}.png?size=#{size}x#{size}&d=#{default_image}",
-                alt: username, class: 'avatar')
+    g = Gravatar.new(email, default_image: 'blank', size: size)
+    h.image_tag(g.url, alt: username, class: 'avatar')
   end
+
 end
