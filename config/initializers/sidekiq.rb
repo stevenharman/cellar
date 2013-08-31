af8c700cd0459c80066a32c7c1391fe04dc21828
Steven Harman <steveharman@gmail.com>
2013-08-31 13:32:07 -0400

    Configure web server ActiveRecord connection pool

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,9 @@
+current_web_concurrency = Proc.new do
+  web_concurrency = ENV['WEB_CONCURRENCY']
+  web_concurrency ||= Puma.respond_to?(:cli_config) && Puma.cli_config.options.fetch(:max_threads)
+  web_concurrency || 16
+end
+
 local_redis_url = Proc.new do
   config_file = File.join(File.expand_path('../..', __FILE__), 'redis.conf')
   port = File.read(config_file).chomp.split.last
@@ -25,4 +31,17 @@ end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDISTOGO_URL'], namespace: 'brewdega-cellar', :size => 1 }
+
+  Rails.application.config.after_initialize do
+    ActiveRecord::Base.connection_pool.disconnect!
+
+    ActiveSupport.on_load(:active_record) do
+      config = Rails.application.config.database_configuration[Rails.env]
+      config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
+      config['pool'] = ENV['WEB_DB_POOL_SIZE'] || current_web_concurrency.call
+      ActiveRecord::Base.establish_connection(config)
+
+      Rails.logger.info("Connection Pool size for web server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
+    end
+  end
 end
