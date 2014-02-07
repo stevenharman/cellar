03884d0408ac8cad67985ecddbfe00b0943b53e1
Steven Harman <steven@harmanly.com>
2014-02-07 14:14:26 -0500

    Add a couple of notes for the future.

--- app/models/search/engine.rb
+++ app/models/search/engine.rb
@@ -4,6 +4,8 @@ module Search
     def self.search(query)
       return Results.empty if query.blank?
 
+      # TODO: Consider an Aggregator to allow eager loading of Brew/Brewery associations
+      # see: http://blog.givegab.com/post/75043413459/using-enumerations-to-make-a-faster-activity-feed-in
       search = PgSearch.multisearch(query.terms, query.options).includes(:searchable)
       search = search.where(searchable_type: query.document_scope) if query.document_scoped?
       search = search.page(query.page) if query.paged?
--- app/serializers/brew_serializer.rb
+++ app/serializers/brew_serializer.rb
@@ -5,6 +5,7 @@ class BrewSerializer < ApplicationSerializer
 
   links do
     link :self, href: urls.brew_url(object)
+    # TODO: Move this to a :labels object
     link :mediumLabel, href: object.medium_image if object.medium_image?
   end
 
